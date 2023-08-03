a005f159c67f492f729ccbe6c5d8f8124b35bd18
marcobaobao <marco.piovanello@gmail.com>
2023-08-03 12:08:22 +0200

    added roboto font

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
 
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
 const root = createRoot(document.getElementById('root')!)
 
 root.render(
