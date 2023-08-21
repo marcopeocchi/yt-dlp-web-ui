598f30c63ce3fecc408f93fe4b766491dd7e4321
marcobaobao <marco.piovanello@gmail.com>
2023-08-21 09:57:18 +0200

    use http.FileServer insetead of custom middleware

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,6 @@
 import react from '@vitejs/plugin-react-swc'
 import ViteYaml from '@modyfi/vite-plugin-yaml'
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 
 export default defineConfig(() => {
   return {
@@ -9,10 +8,8 @@ export default defineConfig(() => {
       react(),
       ViteYaml(),
     ],
-    root: resolve(__dirname, '.'),
     build: {
       emptyOutDir: true,
-      outDir: resolve(__dirname, 'dist'),
     }
   }
 })
