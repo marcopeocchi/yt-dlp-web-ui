c78b3ae174e300860e2644e6d0d0c3a90f338cef
marcobaobao <marco.piovanello@gmail.com>
2024-01-16 13:24:08 +0100

    introduced millionjs compiler

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,12 @@
 import react from '@vitejs/plugin-react-swc'
+import million from 'million/compiler'
 import ViteYaml from '@modyfi/vite-plugin-yaml'
 import { defineConfig } from 'vite'
 
 export default defineConfig(() => {
   return {
     plugins: [
+      million.vite({ auto: true }),
       react(),
       ViteYaml(),
     ],
