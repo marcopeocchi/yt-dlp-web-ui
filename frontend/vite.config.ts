ac077ea1e189f0413755e3cb76809bc1f7f8c32a
Marco Piovanello <35533749+marcopeocchi@users.noreply.github.com>
2024-12-12 09:49:20 +0100

    upgraded react to v19 (#232)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,10 @@
 import react from '@vitejs/plugin-react-swc'
-import million from 'million/compiler'
 import ViteYaml from '@modyfi/vite-plugin-yaml'
 import { defineConfig } from 'vite'
 
 export default defineConfig(() => {
   return {
     plugins: [
-      million.vite({ auto: true }),
       react(),
       ViteYaml(),
     ],
